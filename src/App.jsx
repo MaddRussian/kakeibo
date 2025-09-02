f828ac883bdf0e745c63283220324edc50ee6378
William Sebastian <okeefesebastian91@gmail.com>
2025-09-02 19:51:24 +0900

    created space for monthly budget and monthly bills

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 import AddExpenseForm from "./components/AddExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 import Summary from "./components/Summary";
+import BudgetForm from "./components/BudgetForm";
 
 function App() {
   const [expenses, setExpenses] = useState(() => {
     const saved = localStorage.getItem("expenses");
     return saved ? JSON.parse(saved) : [];
   });
+
+  const [budget, setBudget] = useState(0);
+  const [bills, setBills] = useState(0);
+
+  let totalExpenses = 0;
+  expenses.forEach((e) => {
+    totalExpenses += e.amount;
+  });
+
+  const available = budget - bills - totalExpenses
 
   useEffect(() => {
     localStorage.setItem("expenses", JSON.stringify(expenses));
@@ -24,6 +35,18 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Kakeibo</h1>
+
+      <BudgetForm
+        budget={budget}
+        setBudget={setBudget}
+        bills={bills}
+        setBills={setBills}
+      />
+      <h3>Your Personal Budget Buddy</h3>
+      <p>
+        Budget: {budget}円 | Bills: {bills}円 | Available: {available}円
+      </p>
+
       <AddExpenseForm onAdd={addExpense} />
       <ExpenseList expenses={expenses} onDelete={handleDeleteExpense} />
       <Summary expenses={expenses} />
---
+++ src/components/BudgetForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function BudgetForm({ setBudget, setBills }) {
+  const [inputBudget, setInputBudget] = useState("");
+  const [inputBills, setInputBills] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInputBudget(Number(inputBudget));
+    setBills(Number(inputBills));
+    setInputBudget("");
+    setInputBills("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="p-4 border rounded">
+      <h2 className="text-lg font-bold mb-2">Set Monthly Budget</h2>
+
+      <input
+        type="number"
+        placeholder="Monthly budget"
+        value={inputBudget}
+        onChange={(e) => setInputBudget(e.target.value)}
+        className="border p-2 mr-2"
+        />
+
+      <input
+        type="number"
+        placeholder="Monthly bills"
+        value={inputBills}
+        onChange={(e) => setInputBills(e.target.value)}
+        className="border p-2 mr-2"
+        />
+
+      <button type="submit" className="bg-blue-500 text-white px-3 py-1 rounded">
+        Save
+      </button>
+
+    </form>
+  );
+}
